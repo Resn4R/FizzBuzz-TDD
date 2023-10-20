a3dd5afc91cbd7b56b3eb99da1db7d5ed569661f
Resn4R <purview-joust.0x@icloud.com>
2023-10-20 18:54:16 +0100

    Initial Commit

---
+++ FizzBuzz/FizzBuzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  FizzBuzzApp.swift
+//  FizzBuzz
+//
+//  Created by Vito Borghi on 20/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct FizzBuzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
